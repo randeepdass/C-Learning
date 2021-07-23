f37f0ee7c7b771203f0beff6feea016e0441a23d
Randeep <randeepjangra687@gmail.com>
2021-07-23 12:47:49 +0530

    C Learning Day 1

---
+++ variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+    int j;
+    printf("Enter the first Number : ");
+    scanf("%d", &i );
+    printf("Enter the second Number : ");
+    scanf("%d", &j );
+    printf("The sum of given Numbers is : %d", i+j );
+    return 0;
+}

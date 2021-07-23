fe059d02e183609c55d6cdf8907068f98656fd92
Randeep <randeepjangra687@gmail.com>
2021-07-23 13:36:13 +0530

    C Learning Day 2

---
+++ Day 2/PracticeSet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    // (Area Of Rectangle)
+    // int length , breadth;
+    // printf("Enter length of the rectangle : ");
+    // scanf(" %d", &length);
+    // printf("Enter the breadth of the rectangle : ");
+    // scanf("%d", &breadth);
+    // printf("The area of rectangle is : %d", length * breadth);
+
+    /* Farhenit to celsius */
+
+    float celsius ,far;
+    printf("Enter your temperature in celsius : ");
+    scanf("%f", &celsius);
+    printf("Your temperature in fahrenheit is : %f", ((celsius * 9 / 5) + 32));
+
+    
+    return 0;
+}
